97f999bc3d59b6b5624b9351d388797ee8fd4217
zdeep10 <gabriel.wti02@gmail.com>
2023-03-21 19:37:12 -0300

    Creating and otimizing post routes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify, {
   FastifyInstance,
   FastifyReply,
   FastifyRequest,
+  RouteGenericInterface,
 } from 'fastify';
 
 import { PrismaClient } from '@prisma/client';
@@ -14,22 +15,75 @@ interface TodoParams {
   id: string;
 }
 
+interface CreateTodoRequest {
+  text: string;
+  userID: string;
+}
+
+interface CreateUserRequest {
+  user: string;
+  email: string;
+  password: string;
+}
+
+interface NewTodoRouteGenericInterface extends RouteGenericInterface {
+  Body: CreateTodoRequest;
+}
+
+// GET METHOD
+
 // GETTING TASKS
 app.get('/todos/:id', async (request, reply) => {
   const { id } = request.params as TodoParams;
 
   const todos = await prisma.tasks.findMany({ where: { userID: id } });
 
-  reply.send(todos);
+  return reply.status(200).send(todos);
 });
 
 // GETTING USERS
 app.get('/users', async (request: FastifyRequest, reply: FastifyReply) => {
   const users = await prisma.userinfos.findMany();
 
-  reply.send(users);
+  return reply.status(200).send(users);
 });
 
+// END GET METHOD
+
+// POST METHOD
+
+// POSTING TASKS
+app.post<NewTodoRouteGenericInterface>(
+  '/todos/new',
+  async (
+    request: FastifyRequest<{ Body: CreateTodoRequest }>,
+    reply: FastifyReply
+  ): Promise<CreateTodoRequest> => {
+    const task = await prisma.tasks.create({
+      data: { text: request.body.text, userID: request.body.userID },
+    });
+
+    return reply.send(task);
+  }
+);
+
+// CREATING USERS
+app.post(
+  '/users/new',
+  async (request: FastifyRequest<{ Body: CreateUserRequest }>, reply) => {
+    const newUser = await prisma.userinfos.create({
+      data: {
+        email: request.body.email,
+        user: request.body.user,
+        password: request.body.password,
+      },
+    });
+
+    return reply.send(newUser)
+  }
+);
+
+// END POST METHOD
 app
   .listen({
     host: '0.0.0.0',
